441f9988f3368d3ba7138b10a8d0a0d6e71880a0
DerangedMango <45147178+DerangedMango@users.noreply.github.com>
2019-05-05 17:11:22 -0400

    Change version to 1.0.0

--- src/main/java/io/github/derangedmango/minecrafthuemod/MinecraftHueMod.java
+++ src/main/java/io/github/derangedmango/minecrafthuemod/MinecraftHueMod.java
@@ -23,7 +23,7 @@ import java.util.Scanner;
 public class MinecraftHueMod {
     public static final String MODID = "minecraft-hue";
     public static final String NAME = "Minecraft Hue";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.0";
 
     private static Logger logger;
     private static File configDir;
